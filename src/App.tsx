7daffe1bff12de1dce5cb7e7b2093cc826340388
Marine Ligny <mligny.dev@gmail.com>
2025-10-26 11:22:31 +0100

    feat(search-products): add Action onSubmit to search

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import ProductSearch from 'features/product/search-products/ui/ProductSearch';
 
 function App() {
-  return <ProductSearch />;
+  const onSubmit = (search: string): void => {
+    console.log('Recherche de produit :', search);
+  };
+  return <ProductSearch onSubmit={onSubmit} />;
 }
 
 export default App;
--- src/features/product/search-products/ui/ProductSearch/ProductSearch.tsx
+++ src/features/product/search-products/ui/ProductSearch/ProductSearch.tsx
@@ -1,16 +1,34 @@
 import type { ReactElement } from 'react';
 import { Search } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import { useState } from 'react';
 
-const ProductSearch = (): ReactElement => {
+type Props = {
+  onSubmit: (search: string) => void;
+};
+
+const ProductSearch = ({ onSubmit }: Props): ReactElement => {
+  const [search, setSearch] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    onSubmit(search);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type='text'
         id='product-search'
         name='product-search'
         placeholder='Rechercher un produit...'
         aria-label='Rechercher un produit'
+        value={search}
+        onChange={handleChange}
       />
       <IconButton type='submit' aria-label='Rechercher'>
         <Search />
